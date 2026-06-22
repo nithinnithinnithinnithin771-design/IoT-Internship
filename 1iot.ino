785df36d450bd253e8e289d24011e70463b11927
nithinnithinnithinnithin771-design <nithinnithinnithinnithin771@gmail.com>
2026-06-22 21:54:37 +0530

    Add files via upload
    smart dustbin using servo and IR

---
+++ 1iot.ino
@@ -0,0 +1,24 @@
+#include<Servo.h>
+#define IR 13
+Servo s;
+void setup() {
+  pinMode(13,INPUT);
+  Serial.begin(115200);
+  s.attach(5);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  int i=digitalRead(13);
+   s.write(90);
+   if(i==0){
+    s.write(90);
+   }
+   else{
+    s.write(0);
+   }
+   delay(1000);
+  // put your main code here, to run repeatedly:
+
+}
